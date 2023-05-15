38b3c426e3f66294d143a778b5f504ee2526e164
ValdRichard <ricardovaldivia2003@gmail.com>
2023-05-15 20:37:48 -0300

    Ayuda

--- target/generated-sources/annotations/com/portfolio_bd/api/Mapper/SkillMapperImpl.java
+++ target/generated-sources/annotations/com/portfolio_bd/api/Mapper/SkillMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 /*
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-05-15T20:01:57-0300",
+    date = "2023-05-15T20:36:49-0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 19.0.2 (Oracle Corporation)"
 )
 */

86ffb235c21dedd4fc90fa11523d04829c43027d
ValdRichard <ricardovaldivia2003@gmail.com>
2023-05-15 20:48:01 -0300

    adsf

--- target/generated-sources/annotations/com/portfolio_bd/api/Mapper/PersonaMapperImpl.java
+++ target/generated-sources/annotations/com/portfolio_bd/api/Mapper/PersonaMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 /*
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-05-15T20:41:26-0300",
+    date = "2023-05-15T20:46:43-0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 19.0.2 (Oracle Corporation)"
 )
 */

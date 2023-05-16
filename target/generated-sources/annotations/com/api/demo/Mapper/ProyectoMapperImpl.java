da9bba1dbf5735cc22f7b9c5a3eedbb14cb2ad03
ValdRichard <ricardovaldivia2003@gmail.com>
2023-05-16 10:54:02 -0300

    Cambio en build2.

--- target/generated-sources/annotations/com/api/demo/Mapper/ProyectoMapperImpl.java
+++ target/generated-sources/annotations/com/api/demo/Mapper/ProyectoMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-05-15T23:10:17-0300",
+    date = "2023-05-16T10:53:09-0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 20.0.1 (Oracle Corporation)"
 )
 @Component
